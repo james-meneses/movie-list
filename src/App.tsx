f2191e253c6bea9f78ad0d53d9aff62ff44b9d25
James Meneses <james.meneseswk@gmail.com>
2021-05-17 14:55:40 -0300

    Separation of Concerns: The Sidebar Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from 'react';
 
 import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
+import { SideBar } from './components/SideBar'
 
 // import { SideBar } from './components/SideBar';
 // import { Content } from './components/Content';
 
 import { api } from './services/api';
+
 
 import './styles/global.scss';
 
@@ -54,28 +56,20 @@ export function App() {
     })
   }, [selectedGenreId]);
 
-  function handleClickButton(id: number) {
+   function handleClickButton(id: number) {
     setSelectedGenreId(id);
   }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
+     
+     {
+       genres.length && 
+       <SideBar genres={genres}
+                handleClickButton={handleClickButton}
+                selectedGenreId={selectedGenreId}
+                 />
+      }
 
       <div className="container">
         <header>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,47 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import { Button } from './Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
 }
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function SideBar(props) {
+  const genres = props.genres
+
+  console.log('genres => ', genres)
+
+   return (
+     <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+
+        <div className="buttons-container">
+          {genres.length && genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => props.handleClickButton(genre.id)}
+              selected={props.selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+      )
+
+   }
